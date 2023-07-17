a2710bd58bd6476238d191570f9d4d1b6f68c0ee
yangabrp <yg@forpeople.io>
2023-07-17 17:33:16 -0300

    feat: create musics endpoint

--- src/controllers/artistsController.ts
+++ src/controllers/artistsController.ts
@@ -4,4 +4,7 @@ export class ArtistsController{
   public artists = (req: Request, res: Response) => {
     res.json({message: "Layne Staley"})
   }
+  public musics = (req: Request, res: Response) => {
+    res.json({message: "god am"})
+  }
 }
--- src/routes/artistsRoute.ts
+++ src/routes/artistsRoute.ts
@@ -1,8 +1,9 @@
-import express from "express"
+import {Router} from "express"
 import { ArtistsController } from "../controllers/artistsController"
 const artistsController = new ArtistsController()
-const router = express.Router()
+const  router = Router()
 
 router.get("/", artistsController.artists)
+router.get("/oi", artistsController.musics)
 
 export default router
